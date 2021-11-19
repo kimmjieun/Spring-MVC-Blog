c66d62366352755c9fe4131bd3ca79370b5085f5
김지은 <kimmjieun@gmail.com>
2021-11-19 16:29:04 +0900

    JPA 리파지터리, 데이터 저장하기

--- src/main/java/com/example/myblog/controller/ArticleApiController.java
+++ src/main/java/com/example/myblog/controller/ArticleApiController.java
@@ -1,7 +1,10 @@
 package com.example.myblog.controller;
 
 import com.example.myblog.dto.ArticleForm;
+import com.example.myblog.entity.Article;
+import com.example.myblog.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class ArticleApiController {
+    @Autowired // 리파지터리 객체를 알아서 가져옴! 자바는 new ArticleRepository() 해야 했음!
+    private ArticleRepository articleRepository;
+
     @PostMapping("/api/articles") // Post 요청이 "/api/articles" url로 온다면, 메소드 수행!
     public Long create(@RequestBody ArticleForm form) { // JSON 데이터를 받아옴!
+
         log.info(form.toString()); // 받아온 데이터 확인!
-        return 0L;
+
+        // dto(데이터-전달-객체)를 entity(db-저장-객체)로 변경
+        Article article = form.toEntity();
+
+        // 리파지터리에게(db-관리-객체) 전달
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+
+        // 저장 엔티티의 id(PK)값 반환!
+        return saved.getId();
     }
 }
--- src/main/java/com/example/myblog/dto/ArticleForm.java
+++ src/main/java/com/example/myblog/dto/ArticleForm.java
@@ -1,39 +1,19 @@
 package com.example.myblog.dto;
 
+import com.example.myblog.entity.Article;
+import lombok.Data;
+
+@Data // 생성자(디폴트, All), 게터, 세터, toString 등 다 만들어 줌!
 public class ArticleForm {
     private String title;
     private String content;
-    private String author;
 
-    public ArticleForm(String title, String content, String author) {
-        this.title = title;
-        this.content = content;
-        this.author = author;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getContent() {
-        return content;
-    }
-    public String getAuthor(){
-        return author;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
-    public void setAuthor(String author){
-        this.author = author;
-    }
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", author= '" + author + '\'' +
-                '}';
+    // 빌더 패턴으로 객체 생성! 생성자의 변형. 입력 순서가 일치하지 않아도 됨.
+    public Article toEntity() {
+        return Article.builder()
+                .id(null)
+                .title(title)
+                .content(content)
+                .build();
     }
 }
