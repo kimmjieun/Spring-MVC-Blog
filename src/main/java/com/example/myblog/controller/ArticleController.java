4d7cfdc37e4066023fcbd57c498b74b9f84f597e
김지은 <kimmjieun@gmail.com>
2021-11-19 15:32:21 +0900

    폼(form) 데이터 주고 받기

--- src/main/java/com/example/myblog/controller/ArticleController.java
+++ src/main/java/com/example/myblog/controller/ArticleController.java
@@ -1,9 +1,13 @@
 package com.example.myblog.controller;
 
+import com.example.myblog.dto.ArticleForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j // 로깅(logging) 기능 추가! Lombok 플러그인 설치 필요!
 @Controller // 컨트롤러 선언! 요청과 응답을 처리!
 public class ArticleController {
     @GetMapping("/articles") // 해당 요청을 처리하도록, 메소드를 등록!
@@ -23,5 +27,11 @@ public class ArticleController {
     @GetMapping("/articles/list") // GET 요청이 해당 url로 오면, 아래 메소드를 수행!
     public String articleList() {
         return "articles/list"; // 보여줄 뷰 페이지
+    }
+
+    @PostMapping("/articles") // Post 방식으로 "/articles" 요청이 들어오면 아래 메소드 수행
+    public String create(ArticleForm form){ // 폼 태그의 데이터가 ArticleForm 객체로 만들어짐!
+        log.info(form.toString()); // ArticleForm 객체 정보를 확인!
+        return "redirect:/articles"; // 브라우저를 "articles" url로 보냄!
     }
 }
---
+++ src/main/java/com/example/myblog/dto/ArticleForm.java
@@ -0,0 +1,39 @@
+package com.example.myblog.dto;
+
+public class ArticleForm {
+    private String title;
+    private String content;
+    private String author;
+
+    public ArticleForm(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", author= '" + author + '\'' +
+                '}';
+    }
+}
